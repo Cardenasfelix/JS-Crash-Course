a835cc85a57972843003be6b0b66e1a1f273d3d8
Cardenasfelix <cardenaslomeli.felix@gmail.com>
2023-08-28 17:37:51 -0700

    Data & Variable Types

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('hello world')
+// Below shows a Data string type.-----//
+console.log('Hello World' [0] + 'World' [4])
+console.log('123458'.length)
+// Below shows a Variable type.------//
+let str = 'Felix'
+console.log(str[str.length -1])
