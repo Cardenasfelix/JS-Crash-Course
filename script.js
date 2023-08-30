1d0050036533eca07ed95338e4a9a2a4b2559a7c
Cardenasfelix <cardenaslomeli.felix@gmail.com>
2023-08-29 21:27:11 -0700

    Practicing on Different Variables

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-// Below shows a Data string type.-----//
-console.log('Hello World' [0] + 'World' [4])
-console.log('123458'.length)
-// Below shows a Variable type.------//
-let str = 'Felix'
-console.log(str[str.length -1])
+// Below will show types of Variables involving VAR, LET & CONST  --//
+let fullName = 'Felix Cardenas'
+let temperature = 20
+let planet = 'Earth'
+// "Let" variable make the value equal to "20" --//
+temperature = temperature + 2
+
+console.log(temperature)
+
+// Once you assign a variable, after you can not reassign another variable to it //
